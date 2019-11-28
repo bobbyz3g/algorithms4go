6230e57b65eed0451e6fae3d3c0f6ef4ab28f94d
Zhang liangliang <zhangliangliang1996@hotmail.com>
2019-11-28 21:02:04 +0800

    :recycle: Rename interface interator and fix comments error.

--- base/Iterator.go
+++ base/Iterator.go
@@ -1,7 +1,7 @@
 package base
 
-// Iterable is a iterator for containers.
-type Iterable interface {
+// Iterator is the interface that wraps the basic iterator methods.
+type Iterator interface {
 	// HasNext returns true if there was a next element, or false if there was not.
 	HasNext() bool
 	// Value returns the current element's value and moves the iterator to the next element.
